1b501b910e08f4b0faae71ac359d57f6bdf0b325
ilyes-guy <ilyes.guy@gmail.com>
2022-09-10 21:41:04 +0100

    efz

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -1,5 +1,11 @@
 <x-layouts.app>
+
+@if (Auth::user())
     
+    <x-add_post />
+@endif
+<x-extra_bar />
+
 @foreach ($post as $item)
     <x-item  :item='$item' :subs_data='$subs_data' :user_data='$user_data'/>
 @endforeach
