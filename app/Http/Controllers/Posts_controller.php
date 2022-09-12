b22346ba7871391174f3ca60385dd3b63253771f
ilyes-guy <ilyes.guy@gmail.com>
2022-09-12 20:18:27 +0100

    u

---
+++ app/Http/Controllers/Posts_controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Posts_controller extends Controller
+{
+    public function create()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+    
+}
