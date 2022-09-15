28eb5914b392e2e79b9044c33ee05206386129dc
ilyes-guy <ilyes.guy@gmail.com>
2022-09-15 20:19:22 +0100

    rezg

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import './bootstrap';
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 import Dropzone from "dropzone";
+import { startCase } from 'lodash';
 
 
 if($('.dropzone')[0]){
@@ -42,6 +43,9 @@ $( "#down_vote, #up_vote" ).on("click", function() {
             _token:$('meta[name="csrf-token"]').attr('content'),
             type: $(this).attr('id'),
             id: $(this).parent().attr('id'),
+         },
+         success : function(data) {            
+           
          }
       })
   })
@@ -151,40 +155,6 @@ $('#main_select').on('change', function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $('#submit_button_images_input').on('click', function(){
     $("#input_image_form").submit()
 })
@@ -221,3 +191,67 @@ $('#join_leave').on('click', function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+}
+var delay = 0
+function start(){
+    if(delay==0){
+        delay = 3000
+        setTimeout(function() { delay = 0 }, 3000);
+        
+        console.log('grege')
+        $('#posts_cont').append('<div id="to_be_removed" role="status" class="m-6 rounded-lg border border-gray-200 shadow animate-pulse md:p-6 dark:border-gray-700"><div class="flex justify-center items-center mb-4 h-48 bg-gray-300 rounded dark:bg-gray-700"><svg class="w-12 h-12 text-gray-200 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path></svg></div><div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div><div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div><div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div><div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div><div class="flex items-center mt-4 space-x-3"><div><div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div><div class="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div></div></div><span class="sr-only">Loading...</span></div>')
+        
+        $.ajax({
+            url : "/load_more",
+            method: 'post',
+            data: {
+                _token:$('meta[name="csrf-token"]').attr('content'),
+                type: $(this).attr('id'),
+                id: $(this).parent().attr('id'),
+             },
+             success : function(data) {            
+               
+             }
+        })
+    }
+}
+//const box = document.querySelector('#divdiv');
+
+//const box = $('#posts_cont').children().last().attr('id');
+const box = document.querySelector('#posts_cont').lastElementChild;
+
+document.addEventListener('scroll', function () {
+    isInViewport(box) ?
+    start() :
+    ''
+}, {
+    passive: true
+})
